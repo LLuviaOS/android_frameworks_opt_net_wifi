dabbac13c9176d66399b6990959265c3f5b8f5a4
Nicholas Lim <nicknitewolf@lineageos.org>
2019-10-06 19:17:19 +0800

    Fix derp in 119f4e61cd2164a56ebc4caba8ec735e36f70422
     * Missed updating WifiInfo for the case where
       LinkLayerStats are broken or unsupported
    
    Change-Id: Id75de2ce43d57d88516db4b5718f0575a1d973fe

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -1279,8 +1279,9 @@ public class ClientModeImpl extends StateMachine {
                 mWifiInfo.updatePacketRates(stats, mLastLinkLayerStatsUpdate);
             }
         } else { // LinkLayerStats are broken or unsupported
-                long mTxPkts = mFacade.getTxPackets(mInterfaceName);
-                long mRxPkts = mFacade.getRxPackets(mInterfaceName);
+            long mTxPkts = mFacade.getTxPackets(mInterfaceName);
+            long mRxPkts = mFacade.getRxPackets(mInterfaceName);
+            mWifiInfo.updatePacketRates(mTxPkts, mRxPkts, mLastLinkLayerStatsUpdate);
         }
         return stats;
     }
